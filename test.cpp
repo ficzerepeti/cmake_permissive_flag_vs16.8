fa38d0b6586f12e046bedfcef938151c21754345
Peter Ficzere <ficzerepeti@gmail.com>
2020-11-12 21:31:33 +0100

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    char* text = "Hello world!"; // Is error with /permissive- but is not with /permissive
+    std::cout << text << std::endl;
     return 0;
 }
